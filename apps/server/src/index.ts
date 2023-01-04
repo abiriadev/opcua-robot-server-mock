874f7f589723493ff58d56d98e16f2502358810a
Abiria <abiria.dev@gmail.com>
2023-01-04 16:06:24 +0900

    feat: implement parser, (only) Object tree generator

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,10 @@
-import { OPCUAServer } from 'node-opcua'
-import { deserializeArray } from 'class-transformer'
-import { readFile } from 'node:fs/promises'
-import { Node } from './node'
-import 'reflect-metadata'
+import { config } from 'dotenv'
+import { Node } from 'node'
+import { Namespace, NodeId, OPCUAServer } from 'node-opcua'
+
+import { parse } from './parse'
+
+config()
 
 const server = new OPCUAServer({
 	port: 4840,
@@ -14,14 +16,39 @@ const server = new OPCUAServer({
 	},
 })
 
-const parser = async () => {
-	return deserializeArray(
-		Node,
-		(await readFile('../../tmp/opcua/flat.json')).toString(),
-		{
-			excludeExtraneousValues: true,
-		},
-	)
+// check whether the given node id exists in current namespace.
+// NOTE: if namespace does not match, it will return true.
+const nodeExists = (
+	ns: Namespace,
+	nid: string | NodeId,
+): boolean => {
+	const nodeId = NodeId.resolveNodeId(nid)
+
+	if (nodeId.namespace !== ns.index) return true
+
+	return ns.findNode(nid) !== null
+}
+
+const rec = (ns: Namespace, node: Node, pnid: string) => {
+	if (!nodeExists(ns, node.nodeId)) {
+		if (node.nodeClass === 'Object') {
+			const nwob = ns.addObject({
+				browseName: node.browseName,
+				organizedBy: pnid,
+				displayName: node.displayName,
+				nodeId: node.nodeId,
+				typeDefinition: node.typeDefinition,
+			})
+
+			console.log(
+				`${node.nodeId} (${
+					node.browseName
+				}) => ${nwob.nodeId.toString()}`,
+			)
+		}
+	}
+
+	node.children.map(child => rec(ns, child, node.nodeId))
 }
 
 void (async () => {
@@ -29,22 +56,20 @@ void (async () => {
 
 	const { addressSpace } = server.engine
 	const ns = addressSpace?.getOwnNamespace()
+	if (ns === undefined) throw new Error('cccccc')
 
-	const arr = await parser()
+	const nodeTree = await parse()
 
-	arr.map(({ browseName, displayName, nodeId, nodeClass }) => {
-		if (nodeId === 'ROOT') return
-		else if (nodeId === 'i=17602') return
-		else if (nodeClass === 'Object') {
-			ns?.addObject({
-				organizedBy: addressSpace?.rootFolder.objects,
-				browseName,
-				displayName,
-			})
-		}
-	})
+	const o = nodeTree.find(n => n.browseName === 'Objects')
+	if (o === undefined) throw new Error('bbbbb')
+	// rec(ns, o)
+	const x = addressSpace?.rootFolder.objects
+	if (x === undefined) throw new Error('xxxxx')
+	o.children.map(ch => rec(ns, ch, o.nodeId))
 
 	await server.start()
 
-	console.log(`server is now listening on ${server.getEndpointUrl()}`)
+	console.log(
+		`server is now listening on ${server.getEndpointUrl()}`,
+	)
 })()
