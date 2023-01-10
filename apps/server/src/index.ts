48259cdc24a431d57d95e87de931a8e88b42e508
Abiria <abiria.dev@gmail.com>
2023-01-10 09:15:01 +0900

    feat: implement basic server features

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,13 +1,7 @@
 import { config } from 'dotenv'
+import { NodeClass, NodeId, NodeIdType, UaNode } from 'node'
 import {
-	Node,
-	NodeClass,
-	NodeId,
-	NodeIdType,
-	ObjectNode,
-	UaNode,
-} from 'node'
-import {
+	DataType,
 	Namespace,
 	NodeIdLike,
 	OPCUAServer,
@@ -44,6 +38,7 @@ const nodeExists = (
 	return ns.findNode(nid) !== null
 }
 
+// convert NodeId to OpcNodeId
 const toNodeId = (nodeId: NodeId): OpcNodeId =>
 	new OpcNodeId(
 		nodeId.identifierType === NodeIdType.NUMERIC
@@ -53,6 +48,19 @@ const toNodeId = (nodeId: NodeId): OpcNodeId =>
 		nodeId.namespace,
 	)
 
+const getDefault = (dataType: DataType): unknown => {
+	switch (dataType) {
+		case DataType.Byte:
+			return null
+		case DataType.String:
+			return 'hello'
+		case DataType.UInt16:
+		case DataType.UInt32:
+		case DataType.UInt64:
+			return 123
+	}
+}
+
 const rec = (
 	ns: Namespace,
 	node: UaNode,
@@ -61,22 +69,53 @@ const rec = (
 	const nodeId = toNodeId(node.nodeId)
 
 	if (!nodeExists(ns, nodeId)) {
-		if (node.nodeClass === NodeClass.Object) {
-			const nwob = ns.addObject({
-				browseName: node.browseName,
-				organizedBy: pnid,
-				displayName: node.displayName,
-				nodeId,
-				// typeDefinition: (
-				// 	node as unknown as ObjectNode
-				// ).typeDefinition,
-			})
+		const shared = {
+			browseName: node.browseName,
+			description: node.description,
+			displayName: node.displayName,
+			nodeId,
+		}
 
-			console.log(
-				`${nodeId} (${
-					node.browseName
-				}) => ${nwob.nodeId.toString()}`,
-			)
+		if (node.nodeClass === NodeClass.Object) {
+			ns.addObject({
+				...shared,
+				organizedBy: pnid,
+				typeDefinition: toNodeId(
+					node.typeDefinition,
+				),
+			})
+		} else if (node.nodeClass === NodeClass.Variable) {
+			ns.addVariable({
+				...shared,
+				organizedBy: pnid,
+				accessLevel: node.accessLevel,
+				arrayDimensions: node.arrayDimensions,
+				dataType: toNodeId(node.dataType),
+				historizing: node.historizing,
+				minimumSamplingInterval:
+					node.minimumSamplingInterval,
+				typeDefinition: toNodeId(
+					node.typeDefinition,
+				),
+				userAccessLevel: node.userAccessLevel,
+				value: (dt => ({
+					dataType: dt,
+					value: getDefault(dt),
+				}))(
+					(x => {
+						if (x === null)
+							throw new Error(
+								`${node.dataType} must to be exist`,
+							)
+						else return x
+					})(
+						ns.addressSpace.findDataType(
+							toNodeId(node.dataType),
+						),
+					).basicDataType,
+				),
+				valueRank: node.valueRank,
+			})
 		}
 	}
 
@@ -88,11 +127,10 @@ void (async () => {
 
 	const { addressSpace } = server.engine
 	const ns = addressSpace?.getOwnNamespace()
-	if (ns === undefined) throw new Error('cccccc')
+	if (ns === undefined) throw new Error()
 
 	const nodeTree = await parse()
 
-	// .map(ch => rec(ns, ch, 'RootFolder'))
 	rec(ns, nodeTree[0], 'RootFolder')
 
 	await server.start()
