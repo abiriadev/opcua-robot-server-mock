cb1a915ee9d4bb5c4c13dea8cb863b34e23b5c91
Abiria <abiria.dev@gmail.com>
2023-01-11 11:01:49 +0900

    feat: embed basic API for manipulating each node.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -15,6 +15,11 @@ import {
 } from 'node-opcua'
 
 import { parse } from './parse'
+import {
+	getAllNodeIds,
+	getAllVariables,
+	updateNode,
+} from './random-update'
 
 config()
 
@@ -155,6 +160,53 @@ void (async () => {
 	rec(ns, nodeTree[0], 'RootFolder')
 
 	await server.start()
+
+	const vars = getAllVariables(server)
+
+	const randomValueForType = (dataType: DataType) => {
+		switch (dataType) {
+			case DataType.String: {
+				return `${Math.random()}`
+			}
+			case DataType.Byte: {
+				return (Math.random() * 10) % 8
+			}
+			case DataType.Float: {
+				return Math.random()
+			}
+			case DataType.Int16:
+			case DataType.Int32:
+			case DataType.Int64:
+			case DataType.UInt16:
+			case DataType.UInt32:
+			case DataType.UInt64: {
+				return Math.floor(Math.random() * 10000)
+			}
+			case DataType.Boolean: {
+				return Math.random() > 0.5
+			}
+			default: {
+				return null
+			}
+		}
+	}
+
+	setInterval(
+		() =>
+			vars.map(v => {
+				const value = randomValueForType(
+					v.dataTypeObj.basicDataType,
+				)
+				console.log(
+					`${v.nodeId}(${v.dataTypeObj.browseName}) : ${value}`,
+				)
+				v.setValueFromSource({
+					dataType: v.dataTypeObj.basicDataType,
+					value,
+				})
+			}),
+		100,
+	)
 
 	console.log(
 		`server is now listening on ${server.getEndpointUrl()}`,
